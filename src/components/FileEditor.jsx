28209e57b3f581c4fef822e56b7c7f0b45c9d94f
Hitesh_Bansal <h.hiteshbansal@gmail.com>
2024-05-16 22:54:55 +0530

    commit

--- src/components/FileEditor.jsx
+++ src/components/FileEditor.jsx
@@ -10,7 +10,7 @@ const FileEditor = () => {
    <>
    <div>
    <div className=' bg-amber-400 text-center text-3xl font-mono p-4 text-yellow-800'>
-      Online Editor to read .MD   
+      Online Editor to Readme.MD   
       <DataFetching setCode={setCode}/>
       {/* <ThemeDropdown theme={theme} setTheme={setTheme} /> */}
 </div>
